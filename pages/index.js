3f406e115547b43ef8859b5f243d886a8bd01efc
Ibrahim Abulaila <abeabulaila@gmail.com>
2023-02-14 16:59:43 -0500

    protected homepage to reroute back to login. google signout functionality verified

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,23 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import { useState } from 'react'
-import { useSession } from 'next-auth/react'
+import { getSession, useSession, signOut } from 'next-auth/react'
 
 
 export default function Home() {
 
   const { data: session } = useSession();
 
+  function handleSignOut(){
+    signOut()
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Home Page</title>
       </Head>
-      {session ? User({session}) : Guest()}
+      {session ? User({session, handleSignOut}) : Guest()}
     </div>
 
   )
@@ -34,7 +38,7 @@ function Guest() {
 }
 
 //Authorize User
-function User({session}) {
+function User({session, handleSignOut}) {
   return (
     <main className='container mx-auto text-center py-20'>
       <h3 className='text-4xl font-bold'> Authorize User Homepage </h3>
@@ -44,11 +48,27 @@ function User({session}) {
       </div>
 
       <div className='flex justify-center'>
-        <button className='mt-5 px-10 py-1 rounded-sm bg-indigo bg-gray-50'>Sign Out</button>
+        <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo bg-gray-50'>Sign Out</button>
       </div>
       <div className='flex justify-center'>
         <Link href={'/profile'} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray'>Profile Page</Link>
       </div>
     </main>
   )
+}
+
+export async function getServerSideProps({req}){
+  const session = await getSession({req})
+    if(!session){
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false
+        }
+      }
+    }
+  return{
+    props: { session }
+
+  }
 }
