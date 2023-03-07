4a80021e289fa8f82fd93f59944dbe0157c3da32
Babe Abulaila <abeabulaila@gmail.com>
2023-03-07 18:25:03 -0500

    console logs

--- pages/index.js
+++ pages/index.js
@@ -36,14 +36,18 @@ function Guest() {
 
 //Authorize User
 function User({ session, handleSignOut }) {
+
+  console.log(session.user)
+  console.log(session.user.name)
+
   return (
     <main className='container mx-auto text-center py-20'>
       <h3 className='text-4xl font-bold'> Authorized User Homepage </h3>
       <div className='details'>
         <h5>{session.user.name}</h5>
         <h5>{session.user.email}</h5>
+      
       </div>
-
       <div className='flex justify-center'>
         <button onClick={handleSignOut} className='mt-5 px-10 py-1 rounded-sm bg-indigo bg-gray-50'>Sign Out</button>
       </div>
@@ -51,6 +55,7 @@ function User({ session, handleSignOut }) {
         <Link href={'/profile'} className='mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray'>Profile Page</Link>
       </div>
     </main>
+    
   )
 }
 
